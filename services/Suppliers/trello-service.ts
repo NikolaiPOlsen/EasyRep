66768f2bcff0bd052437341b5352cc166401b325
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-15 12:14:25 +0200

    Updated createTrelloCard function to include authorization in headers and improve API request structure

--- services/Suppliers/trello-service.ts
+++ services/Suppliers/trello-service.ts
@@ -1,11 +1,16 @@
 export const createTrelloCard = async ({ idList, name, desc }: { idList: string, name: string, desc: string }) => {
     const key = process.env.EXPO_PUBLIC_TRELLO_KEY;
     const token = process.env.EXPO_PUBLIC_TRELLO_TOKEN;
-    const response = await fetch(`https://api.trello.com/1/cards?key=${key}&token=${token}&idList=${idList}&name=${encodeURIComponent(name)}&desc=${encodeURIComponent(desc)}`, {
+    
+    const response = await fetch(`https://api.trello.com/1/cards?idList=${idList}&name=${encodeURIComponent(name)}&desc=${encodeURIComponent(desc)}`, {
         method: 'POST',
-        headers: { "Content-Type": "application/json" },
+        headers: { 
+            "Content-Type": "application/json",
+            "Authorization": `OAuth oauth_consumer_key="${key}", oauth_token="${token}"`
+        },
         body: JSON.stringify({ idList: idList, name: name, desc: desc })
     }
 );
+
 return await response.json();
 };
