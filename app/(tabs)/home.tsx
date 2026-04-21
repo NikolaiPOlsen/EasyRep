3442e04af2fb2701ee8d0818b9822c1d7a66493b
Nikolai Pedersen Olsen <Nikolai.polsen@gmail.com>
2026-04-21 20:09:04 +0200

    Refactored HomeScreen to use FunctionButton for consistency and improved layout handling with SafeAreaInsets

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,29 +1,56 @@
 import AppButton from '@/src/components/ui/app-button';
-import { ThemedView } from '@/src/components/ui/themed-view';
+import { FunctionButton } from '@/src/components/ui/app-button';
 import { Colors } from '@/src/constants/theme';
+import { useAuthContext } from '@/src/hooks/use-auth-context';
 import * as expo from 'expo-router';
-import { Dimensions, StyleSheet, useColorScheme } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useWindowDimensions, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
     const colorScheme = useColorScheme();
     const themeColors = Colors[colorScheme ?? 'light'];
+    const { profile } = useAuthContext();
+
+    const insets = useSafeAreaInsets();
+    const { width, height } = useWindowDimensions();
+    const isPortrait = height > width;
+
+    if (isPortrait) {
+        return (
+            <View style={[styles.container, {backgroundColor: themeColors.background}]}>
+                <AppButton label='Ny ordre' onPress={() => expo.router.push('/screens/new-order')} />
+                <AppButton label='Registrer kunde' onPress={() => expo.router.push('/screens/register-customer')} />
+                <AppButton label='Profil' onPress={() => expo.router.push('/screens/user-profile')} />
+            </View>
+        )
+    }
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: themeColors.background }}>
-                <ThemedView style={styles.container}>
-                        <AppButton label='Ny ordre' onPress={() => expo.router.push('/screens/new-order')} />
-                        <AppButton label='Registrer kunde' onPress={() => expo.router.push('/screens/register-customer')} />
-                </ThemedView>
-        </SafeAreaView>
+        <View style={[styles.container, {backgroundColor: themeColors.background}]}>
+            <Text style={[styles.sectionTitle, { color: themeColors.text }]}>Dashboard</Text>
+
+            <View style={[{flexDirection: 'row', gap: 15, marginBottom: 15}]}>
+                <FunctionButton label='Ny ordre' icon='app-registration' onPress={() => expo.router.push('/screens/new-order')} />
+                <FunctionButton label='Registrer kunde' icon='person-add-alt-1' onPress={() => expo.router.push('/screens/register-customer')} />
+                <FunctionButton label='Profil' icon='settings' onPress={() => expo.router.push('/screens/user-profile')} />
+            </View>
+        </View>
     )
 }
 
-const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create ({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
+    sectionTitle: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginBottom: 4,
+    },
+    sectionSubtitle: {
+        fontSize: 16,
+        marginBottom: 28,
+    }
 });
